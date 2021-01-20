86c20ceec4954fb8ad02e8be11d763bff48cae94
Gabe de la Cruz <gvdelacruz@nic.edu>
2021-01-19 23:02:44 -0800

    1/20 lecture code

---
+++ 02-ch02/test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int number1 = 0;
+    // int number2 = 23, number3 = 50;
+
+    // cout << "Hello, there";
+    // cout << "Hello, there";
+    // cout << "Hello, there" << endl;
+    // cout << "Hello,\nthere\n";
+    // cout << "\\" << "\n";
+    // cout << number2 << endl;
+
+    // int age = 0, number2 = 0, number3 = 0;       // Define age as an integer variable 
+    // age = 17;      // Assigns 17 to age
+    // cout << age << " ";   // Displays 17
+    // age = 18;      // Now age is 18
+    // cout << age << '\n';   // Displays 18
+
+    cout << 'A' << '\n';
+    cout << 1234;
+    cout << '\n' << 34.234;
+
+    return 0;
+}
